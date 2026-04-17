266a6ae70ae44d6f30ed5d9aa7ea0d41c33e971f
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-04-17 10:18:55 +0900

    first commit

---
+++ src/main/java/boostcampsnu/gwanakbnbserver/GwanakBnbServerApplication.java
@@ -0,0 +1,13 @@
+package boostcampsnu.gwanakbnbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwanakBnbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwanakBnbServerApplication.class, args);
+    }
+
+}
